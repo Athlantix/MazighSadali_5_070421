648ac6e36c671e53f2ae5da320b8ff83714a8294
Athlantix <mazigh.sadali@gmail.com>
2021-05-03 21:39:40 +0200

    indentation

--- details_produit.js
+++ details_produit.js
@@ -15,9 +15,9 @@ apiUrl.then(data=>{
     const creaDescription=document.createElement('p');
     const creaId=document.createElement('a');
     const creaBouton=document.createElement('button');
-    const creaQuanititePlus=document.createElement('button');
-    const creaQuanititeMoins=document.createElement('button');
-    const creaQuanitite=document.createElement('p');
+    const creaQuantitePlus=document.createElement('button');
+    const creaQuantiteMoins=document.createElement('button');
+    const creaQuantite=document.createElement('p');
     const creaSelect=document.createElement('select');
     const creaCouleur=document.createElement('p');
     const details=document.getElementById('details');
@@ -27,9 +27,9 @@ apiUrl.then(data=>{
     details.appendChild(creaTitre);
     details.appendChild(creaDescription);
     details.appendChild(creaId);
-    details.appendChild(creaQuanitite);
-    details.appendChild(creaQuanititePlus);
-    details.appendChild(creaQuanititeMoins);
+    details.appendChild(creaQuantite);
+    details.appendChild(creaQuantitePlus);
+    details.appendChild(creaQuantiteMoins);
     details.appendChild(creaCouleur);
     details.appendChild(creaSelect);
     details.appendChild(creaPrix);
@@ -44,28 +44,26 @@ apiUrl.then(data=>{
     creaPrix.textContent="Prix: "+data.price.toString().substr(0,2)+","+data.price.toString().substr(2)+" $";
     creaId.href="produit.html?"+data._id;
     creaId.textContent="Acheter";
-    creaQuanititePlus.textContent="+";
-    creaQuanititeMoins.textContent="-";
-    creaQuanitite.textContent="Quantité: "+quantite;
+    creaQuantitePlus.textContent="+";
+    creaQuantiteMoins.textContent="-";
+    creaQuantite.textContent="Quantité: "+quantite;
     creaCouleur.textContent="Couleur";
     creaPrix.id="prix";
-    creaQuanitite.id="quantite";
+    creaQuantite.id="quantite";
 
     //Gestion quantité/prix des boutons "+"" et "-"
-    creaQuanititePlus.addEventListener("click",function(){
+    creaQuantitePlus.addEventListener("click",function(){
         quantite++;
         let calcul=prix*quantite;
-      creaPrix.textContent="Prix: "+calcul/100+","+data.price.toString().substr(2)+" $";
-      creaQuanitite.textContent="Quantité: "+quantite;
+        calculer(prix,quantite,creaPrix,creaQuantite,calcul/100,quantite);
 
     });
     
-    creaQuanititeMoins.addEventListener("click",function (){
+    creaQuantiteMoins.addEventListener("click",function (){
       if(quantite>0){
           quantite--;
           let calcul=prix*quantite;
-          creaPrix.textContent="Prix: "+Math.max(calcul/100,0)+","+data.price.toString().substr(2)+" $";
-          creaQuanitite.textContent="Quantité: "+Math.max(quantite,0);
+          calculer(prix,quantite,creaPrix,creaQuantite,Math.max(calcul/100,0),Math.max(quantite,0));
         }
         else{quantite=0;};
     });
@@ -105,53 +103,7 @@ apiUrl.then(data=>{
         option:selectionOption,
       };
 
-    //récupération produit localStorage
-    let produitRecup=JSON.parse(localStorage.getItem("produit"));
-    //vérification de la présence du même produit, sinon envoi
-    if(produitRecup){
-        let test=0;
-        let test2=0;
-        let modif=0;
-        let test3=0;
-        for (let k=0;k<produitRecup.length;k++){
-
-            if(produitRecup[k].nom!=produitPanier.nom){
-              console.log("pas egal");
-              test3=1;
-            }
-          
-            else if(produitRecup[k].nom==produitPanier.nom
-              && produitRecup[k].quantite==produitPanier.quantite
-              && produitRecup[k].option==produitPanier.option){
-              test=1;
-              console.log("pareil"); 
-            }
-
-            else if(produitRecup[k].nom==produitPanier.nom &&
-              produitRecup[k].quantite!=produitPanier.quantite ||
-              produitRecup[k].option!=produitPanier.option){
-                test2=1;
-                modif=k;
-
-                console.log("modif");
-            }       
-        }
-
-        console.log("test3"+test3)
-        if (test==0 && test2==1 ){
-          produitRecup.splice(modif,1);
-          pushLocal(produitRecup,produitPanier);
-        }
-        else if( test3==1 && test==0){
-          pushLocal(produitRecup,produitPanier);
-        } 
-     }
-
-      else {
-        produitRecup=[];
-        pushLocal(produitRecup,produitPanier);
-      
-      }
+      verifLocalStorage(produitPanier);
     })
 })
 .catch(function() {
@@ -163,4 +115,60 @@ apiUrl.then(data=>{
 function pushLocal(produitRecup,produitPanier){
   produitRecup.push(produitPanier);
   localStorage.setItem("produit",JSON.stringify(produitRecup));
+}
+
+//fonction pour calcul des prix et implémentation dans le DOM
+function calculer(prix,quantite,creaPrix,creaQuantite,option1,quantite){
+
+        creaPrix.textContent="Prix: "+option1+","+prix.toString().substr(2)+" $";
+        creaQuantite.textContent="Quantité: "+quantite;
   }
+  
+
+//fonction verification localStorage
+function verifLocalStorage(produitPanier){
+
+      //récupération produit localStorage
+      let produitRecup=JSON.parse(localStorage.getItem("produit"));
+      //vérification de la présence du même produit, sinon envoi
+      if(produitRecup){
+          let test=0;
+          let test2=0;
+          let modif=0;
+          let test3=0;
+          for (let k=0;k<produitRecup.length;k++){
+  
+              if(produitRecup[k].nom!=produitPanier.nom){
+                test3=1;
+              }
+            
+              else if(produitRecup[k].nom==produitPanier.nom
+                && produitRecup[k].quantite==produitPanier.quantite
+                && produitRecup[k].option==produitPanier.option){
+                test=1;
+              }
+  
+              else if(produitRecup[k].nom==produitPanier.nom &&
+                produitRecup[k].quantite!=produitPanier.quantite ||
+                produitRecup[k].option!=produitPanier.option){
+                  test2=1;
+                  modif=k;
+              }       
+          }
+  
+          if (test==0 && test2==1 ){
+            produitRecup.splice(modif,1);
+            pushLocal(produitRecup,produitPanier);
+          }
+          else if( test3==1 && test==0){
+            pushLocal(produitRecup,produitPanier);
+          } 
+       }
+  
+        else {
+          produitRecup=[];
+          pushLocal(produitRecup,produitPanier);
+        
+        }
+
+}
